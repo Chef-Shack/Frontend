d730c8fd010de4f0bed0064fb1d6d0d18f1656ce
egg <eggboissaye@gmail.com>
2022-10-22 13:12:02 -0400

    Created UserAuthPage component for login and signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./components/pages/HomePage"
+import HomePage from "./components/pages/HomePage";
+import RecipesPage from "./components/pages/RecipesPage";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./css/all-page-styles.css"
+import "./css/all-page-styles.css";
+import UserAuthPage from "./components/pages/UserAuthPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<UserAuthPage type="login"/>}/>
+        <Route path='/signup' element={<UserAuthPage type="signup"/>}/>
+        <Route path='/recipes' element={<RecipesPage/>}/>
       </Routes>
     </Router>
   );
--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -10,7 +10,8 @@ const MainNavbar = () => {
         </NavbarBrand>
 
         <Nav className="ms-auto">
-          <NavLink><p>Login</p></NavLink>
+          <NavLink href="recipes"><p>Recipes</p></NavLink>
+          <NavLink href="login"><p>Login</p></NavLink>
         </Nav>
       </Container>
     </Navbar>
