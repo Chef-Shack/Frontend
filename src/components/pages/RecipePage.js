2a285a22ae738d286c41d6c1c71e23545d6615e0
Ismael <eggboissaye@gmail.com>
2022-11-30 20:50:02 -0500

    Added loading screen while fetching backend data

--- src/components/pages/RecipePage.js
+++ src/components/pages/RecipePage.js
@@ -8,17 +8,20 @@ import starOutline from "../../images/star-outline.png";
 import starFilled from "../../images/star-filled.png";
 
 import "../../css/individual-recipe-page-styles.css";
+import { Spinner } from "react-bootstrap";
 
 const RecipePage = () => {
-  const [title, setTitle] = useState("Invalid Recipe");
-  const [author, setAuthor] = useState("Invalid User");
-  const [image, setImage] = useState("Invalid Image");
-  const [pubDate, setPubDate] = useState("Invalid Date");
-  const [description, setDescription] = useState("Invalid Description");
-  const [ingredients, setIngredients] = useState(["Invalid Ingredients"]);
-  const [procedure, setProcedure] = useState(["Invalid Procedure"]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [image, setImage] = useState("");
+  const [pubDate, setPubDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [ingredients, setIngredients] = useState([""]);
+  const [procedure, setProcedure] = useState([""]);
 
   const [starImage, setStarImage] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   let { id } = useParams();
 
@@ -59,7 +62,6 @@ const RecipePage = () => {
         );
 
         const bodyFormData = new FormData();
-        //TODO: make a check to see if the user is logged in
         bodyFormData.append(
           "username",
           window.localStorage.getItem("username")
@@ -74,7 +76,6 @@ const RecipePage = () => {
           });
       }
 
-      //TODO: set a condition for if the user is not authenticated
     } else {
       if (window.localStorage.getItem("authenticated")) {
         setStarImage(false);
@@ -124,81 +125,92 @@ const RecipePage = () => {
 
       const username = window.localStorage.getItem("username");
 
-      axios.get(`http://localhost:8000/members/get_user_by_name/${username}`).then((res) => {
-        for (const recipeId of res.data.likedRecipes) {
-          if(recipeId == id) {
-            // fill in the like button if the recipe is already liked
-            setStarImage(true)
-            console.log("recipe id matches")
+      axios
+        .get(`http://localhost:8000/members/get_user_by_name/${username}`)
+        .then((res) => {
+          for (const recipeId of res.data.likedRecipes) {
+            if (recipeId == id) {
+              // fill in the like button if the recipe is already liked
+              setStarImage(true);
+              console.log("recipe id matches");
+            }
           }
-        }
-      })
+        }).finally(() => {
+          // since all axios calls are done at this point, set isLoading to false
+          setIsLoading(false);
+        });
     }
 
     getRecipe();
-    checkIfRecipeIsLiked()
+    checkIfRecipeIsLiked();
   }, []);
 
-  return (
-    <>
-      <MainNavbar />
+  if (!isLoading) {
+    return (
+      <>
+        <MainNavbar />
 
-      <div className="recipe-page-content">
-        <h1 className="recipe-page-title">{title}</h1>
-        <p>by {author.toUpperCase()}</p>
+        <div className="recipe-page-content">
+          <h1 className="recipe-page-title">{title}</h1>
+          <p>by {author.toUpperCase()}</p>
 
-        <div className="recipe-page-like-section">
-          <p className="recipe-page-like-text">Liked this recipe?</p>
-          <button
-            className="recipe-page-like-button"
-            onClick={() => {
-              likeButtonHandler();
-            }}
-          >
-            <img
-              className="recipe-page-like-image"
-              src={!starImage ? starOutline : starFilled}
-              alt="star"
-              height={32}
-              width={32}
-            ></img>
-          </button>
+          <div className="recipe-page-like-section">
+            <p className="recipe-page-like-text">Liked this recipe?</p>
+            <button
+              className="recipe-page-like-button"
+              onClick={() => {
+                likeButtonHandler();
+              }}
+            >
+              <img
+                className="recipe-page-like-image"
+                src={!starImage ? starOutline : starFilled}
+                alt="star"
+                height={32}
+                width={32}
+              ></img>
+            </button>
+          </div>
+
+          <p className="recipe-page-date">{getCleanDate(pubDate)}</p>
+
+          <img
+            className="recipe-page-image"
+            src={image}
+            alt={title}
+            height={512}
+            width={512}
+          ></img>
+
+          <div className="recipe-page-desc-container">
+            <p className="recipe-page-desc">{description}</p>
+          </div>
+
+          <div className="recipe-page-section">
+            <h2>Ingredients</h2>
+            <ol>
+              {ingredients.map((x) => (
+                <li>{x}</li>
+              ))}
+            </ol>
+          </div>
+
+          <div className="recipe-page-section">
+            <h2>Instructions</h2>
+            <ol>
+              {procedure.map((x) => (
+                <li>{x}</li>
+              ))}
+            </ol>
+          </div>
         </div>
-
-        <p className="recipe-page-date">{getCleanDate(pubDate)}</p>
-
-        <img
-          className="recipe-page-image"
-          src={image}
-          alt={title}
-          height={512}
-          width={512}
-        ></img>
-
-        <div className="recipe-page-desc-container">
-          <p className="recipe-page-desc">{description}</p>
-        </div>
-
-        <div className="recipe-page-section">
-          <h2>Ingredients</h2>
-          <ol>
-            {ingredients.map((x) => (
-              <li>{x}</li>
-            ))}
-          </ol>
-        </div>
-
-        <div className="recipe-page-section">
-          <h2>Instructions</h2>
-          <ol>
-            {procedure.map((x) => (
-              <li>{x}</li>
-            ))}
-          </ol>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <Spinner id="spinner" animation="border" variant="success" />
+    )
+  }
 };
 
 export default RecipePage;
--- src/components/pages/RecipesPage.js
+++ src/components/pages/RecipesPage.js
@@ -1,40 +1,62 @@
 import MainNavbar from "../MainNavbar";
 import RecipeTile from "../RecipeTile";
+import { Spinner } from "react-bootstrap";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import "../../css/recipe-page-styles.css"
+import "../../css/recipe-page-styles.css";
 
-const RecipesPage = () => { 
-    let [recipes, setRecipes] = useState([])
+const RecipesPage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    async function fillRecipeList() {
-        await axios.get("http://localhost:8000/recipes/get_all_recipes")
-        .then((res) => {
-            setRecipes(res.data.recipes)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
+  async function fillRecipeList() {
+    await axios
+      .get("http://localhost:8000/recipes/get_all_recipes")
+      .then((res) => {
+        setRecipes(res.data.recipes);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        // since all axios calls are done at this point, set isLoading to false
+        setIsLoading(false);
+      });
+  }
 
-    useEffect(() => {
-        fillRecipeList()
-    }, [])
-    
+  useEffect(() => {
+    fillRecipeList();
+  }, []);
+
+  if (!isLoading) {
     return (
-        <>
-            <MainNavbar/>
-            <h1 className="recipe-page-header">Explore Recipes</h1>
-            {/*Todo: make the span a link*/}
-            <p className="recipe-page-subheader">or search by <span className="recipe-page-subheader-span">cuisine</span></p>
-            
-            <div className="recipe-tiles">
-                {recipes.map(recipe => <RecipeTile title={recipe.recipe_title} author={recipe.author} image={recipe.image} id={recipe.id} likes={recipe.likes}/>)}
-            </div>
-                
-        </>
-    )
-}
+      <>
+        <MainNavbar />
+        <h1 className="recipe-page-header">Explore Recipes</h1>
+        {/*Todo: make the span a link*/}
+        <p className="recipe-page-subheader">
+          or search by{" "}
+          <span className="recipe-page-subheader-span">cuisine</span>
+        </p>
+
+        <div className="recipe-tiles">
+          {recipes.map((recipe) => (
+            <RecipeTile
+              title={recipe.recipe_title}
+              author={recipe.author}
+              image={recipe.image}
+              id={recipe.id}
+              likes={recipe.likes}
+            />
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    return <Spinner id="spinner" animation="border" variant="success" />;
+  }
+};
 
 export default RecipesPage;
