9bc3abe744ebc26bde37a95e10b5b93accb4037d
Egg <88744505+developer-egg@users.noreply.github.com>
2023-01-20 16:44:32 -0500

    Update HomePage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -8,7 +8,6 @@ const HomePage = () => {
       <MainNavbar />
 
       <div className="body-content text-center">
-        <button onClick={() => {
         <h1 className="header">Find and Share Recipes With <span className="header-span">ChefShack</span></h1>
         <p className="desc">Chefshack is a community where everyone from home cooks to professional chefs come together to share and discover recipes.</p>
         <Button href="recipes" className="custom-button" id="button" variant="outline-success">Get Started</Button>
