a1c9e846b9c9ea54e9a6d56a0f7b041fbaa43baf
Egg <88744505+developer-egg@users.noreply.github.com>
2023-01-20 16:16:40 -0500

    test button

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -8,6 +8,12 @@ const HomePage = () => {
       <MainNavbar />
 
       <div className="body-content text-center">
+        <button onClick={() => {
+  fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+  }}>Test</button>
+    
         <h1 className="header">Find and Share Recipes With <span className="header-span">ChefShack</span></h1>
         <p className="desc">Chefshack is a community where everyone from home cooks to professional chefs come together to share and discover recipes.</p>
         <Button href="recipes" className="custom-button" id="button" variant="outline-success">Get Started</Button>
