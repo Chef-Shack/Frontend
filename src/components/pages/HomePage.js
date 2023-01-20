48603ba605f4b9e518db5a1c33c65d993a4ccf35
Egg <88744505+developer-egg@users.noreply.github.com>
2023-01-20 16:44:16 -0500

    Update HomePage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -9,11 +9,6 @@ const HomePage = () => {
 
       <div className="body-content text-center">
         <button onClick={() => {
-  fetch('http://159.65.224.118:8080/recipes/get_all_recipes')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-  }}>Test</button>
-    
         <h1 className="header">Find and Share Recipes With <span className="header-span">ChefShack</span></h1>
         <p className="desc">Chefshack is a community where everyone from home cooks to professional chefs come together to share and discover recipes.</p>
         <Button href="recipes" className="custom-button" id="button" variant="outline-success">Get Started</Button>
